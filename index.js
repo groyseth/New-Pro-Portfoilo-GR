c764a371391a04ebd4d6a68b18af45175d9066b9
Grant Royseth <groyseth@gmail.com>
2022-02-17 22:17:56 -0600

    working on group project tab

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+
+
+$(".customed").click(function(){
+    $('tab-link.active').removeClass('active');
+   $('this').addClass('active');
+});
+// var fixing = function(event)  {
+//     console.log($(event.target.tab));
+// // window.EventTarget
+// }
+// fixing();
+
+// $('.customed').on('click', '.pag-button', function(event) {
+//     goGet($(event.target).attr('data-request'))
+//     console.log ($(event.target).attr('data-request'));
+// });
